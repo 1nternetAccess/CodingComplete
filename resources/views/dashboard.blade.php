0d63603b7420502b539354490cc7009aca711e71
Hunter <hunterbench@gmail.com>
2025-10-29 14:33:55 +0900

    Update Dashboard

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href={{asset("css/dashboard.css")}}>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul class="navbar">
+                <li><a>Home</a></li>
+                <li><a>Level Select</a></li>
+                <li><a>Progress</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+    </main>
+</body>
+</html>
