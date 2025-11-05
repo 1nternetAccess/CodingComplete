e7d2a71c58ef33e59bca29d48161cb377f17193b
Hunter <hunterbench@gmail.com>
2025-11-05 14:52:09 +0900

    Add Editor

---
+++ resources/views/editor.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Editor</title>
+  <link rel="stylesheet" href={{asset("css/editor.css")}} />
+</head>
+<body>
+    <header></header>
+
+    <main>
+        <div class="container-1">
+            <div class="container-2">
+                <div class="description">
+                    <h2>Title</h2>
+                    <p>Description</p>
+                </div>
+            </div>
+            <div class="container-2">
+                <div class="editor-area">
+                    <div class="editor">editor</div>
+                    <div class="console">console</div>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
