b9417988b0019eac2e098564afc343bc19139039
DavidKeisuke <atsuabya422@gmail.com>
2025-11-07 14:45:44 +0900

    Cahnged css

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,19 +9,33 @@
     <link rel="stylesheet" href={{asset("css/dashboard.css")}}>
 </head>
 <body>
-    <header><span>Dashboard</span></header>
+    <header><span><h1>Dashboard<h1></span></header>
     <nav>
         <ul class="navbar">
-            <li class="nav-item"><a class="nav-link" href="#"><div class="center-text">Home</div></a></li>
+            <li class="nav-item"><a class="nav-link" href="{{ url('/') }}"><div class="center-text">Home</div></a></li>
             <li class="nav-item"><a class="nav-link" href="#"><div class="center-text">Level Select</div></a></li>
             <li class="nav-item"><a class="nav-link" href="#"><div class="center-text">Progress</div></a></li>
         </ul>
     </nav>
     <main class="main-content">
-        <div class="display-area">
-            <h1>Progress</h1>
-            <p></p>
-        </div>
+
+        <div class="progress">
+            <h2>Progress</h2>
+          
+            <div class="content-box">
+              <div class="intro">Content1</div>
+              <div class="posts">Content2</div>
+            </div>
+          </div>
+
+          <div class="selection">
+            <h2>Selection</h2>
+          
+            <div class="content-box2">
+              <div class="intro2">Content1</div>
+              <div class="posts2">Content2</div>
+            </div>
+          </div>
     </main>
 </body>
 </html>
